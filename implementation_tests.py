91ff4e716a36e2b292bd672183af034330be9a91
Nick <nick.korovesis@soundtalks.com>
2022-03-20 17:45:51 +0100

    add first project files

---
+++ implementation_tests.py
@@ -0,0 +1,22 @@
+from math import sqrt, exp
+import cv2 as cv
+
+
+list_of_functions = [abs, sqrt, exp]
+
+x = iter(list_of_functions)
+
+print(next(x)(-13))
+
+
+print(cv.cvtColor)
+
+y = []
+y.append(cv.cvtColor)
+y.append(cv.cvtColor)
+y.append(abs)
+
+print(y)
+
+t = iter(y)
+print(next(t))
